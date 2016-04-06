5dc9361b017405c0c2f6538f737dc699f7ea380b
Joe Chasinga <jo.chasinga@gmail.com>
2016-04-06 11:26:48 -0400

    Update and fix

--- README.md
+++ README.md
@@ -10,14 +10,37 @@ HTTP [middlewares](https://justinas.org/writing-http-middleware-in-go/) which
 wrap around the target server's handler to simulate latent connections, proxy 
 servers, or load balancers.
 
+Usage
+-----
+To use `relay` in your test, install with 
+
+```bash
+
+$ go get github.com/jochasinga/relay
+
+```
+
+Or better, use a [package manager](https://github.com/golang/go/wiki/PackageManagementTools) like [Godep](https://github.com/tools/godep) or [Glide](https://glide.sh/).
+
+HTTPTestServer
+--------------
+Every instance, including the standard `httptest.Server`, implements `HTTPTestServer` 
+interface, which means that they can be used interchangeably as a general "server".
+
 Proxy
 -----
-
 A `Proxy` is used to place in front of any `HTTPTestServer` (`httptest.Server`,
 `Proxy`, or `Switcher`) to simulate a proxy server or a connection with some
 network latency. It takes a latency unit in `time.Duration` and a backend 
 `HTTPTestServer` as arguments.
 
+Switcher
+--------
+`Switcher` works similarly to `Proxy`, except with each request it "switches" between 
+several backend `HTTPTestServer` in a round-robin fashion.
+
+Examples
+--------
 Let's begin setting up a basic `httptest.Server` to send a request to.
 
 ```go
@@ -37,21 +60,33 @@ func TestGet(t *testing.T) {
 
 ```
 
-Now let's simulate a slow connection through which an HTTP request
-can be sent to the previous test server with `Proxy`.
+Now let's use `Proxy` to simulate a slow connection through which an HTTP request
+can be sent to test server.
 
 ```go
 
-delay := time.Duration(20) * time.Second
+// Connection takes 4s to and from the test server.
+delay := time.Duration(2) * time.Second
+// Client takes 3s before timing out.
+timeout := time.Duration(3) * time.Second
+// Create a new proxy with the delay and test server backend.
 conn := relay.NewProxy(delay, ts)
-client := &Client{Timeout: time.Duration(10) * time.Second}
-resp, _ := client.Get(conn.URL)
+client := &Client{ Timeout: timeout }
+start := time.Now()
+_, _ := client.Get(conn.URL)
+elapsed := time.Since(start)
+deviation := time.Duration(50) * time.Millisecond
+
+if elapsed >= timeout + deviation {
+	    t.Error("Client took too long to time out.")
+}
 
 ```
-Note that the latency will double because it takes a round trip to and 
+Note that the latency will double because of the round trip to and 
 from the server.
 
-`Proxy` can be placed in front of another proxy, and vice versa.
+`Proxy` can be placed in front of another proxy, and vice versa. So you 
+can create a chain of test proxies this way:
 
 ```go
 
@@ -60,17 +95,21 @@ ts := httptest.NewServer(http.HandlerFunc(handler))
 p3 := relay.NewProxy(delay, ts)
 p2 := relay.NewProxy(delay, p3)
 p1 := relay.NewProxy(delay, p2)
-resp, _ := client.Get(p1.URL)
+start := time.Now()
+_, _ := client.Get(p1.URL)
+elapsed := time.Since(start)
+deviation := time.Duration(50) * time.Millisecond
+if elapsed >= (time.Duration * 6) + deviation {
+	    t.Error("Client took longer than expected.")
+}
 
 ```
 
 In the above code, Each hop to and from the target server `ts` will be delayed 
 for one second, total to six seconds of latency.
 
-Switcher
---------
-
-`Switcher` works similarly to a proxy, except with each request it "switches" between several backend servers in a round-robin fashion.
+`Switcher` can be used instead of `Proxy` to simulate a round-robin load-balancing proxy
+or just to switch between several test servers' handlers for convenience.
 
 ```go
 
@@ -91,11 +130,11 @@ sw := relay.NewSwitcher(delay, []HTTPTestServer{ts1, ts2, p})
 resp1, _ := http.Get(sw.URL) // hits ts1
 resp2, _ := http.Get(sw.URL) // hits ts2
 resp3, _ := http.Get(sw.URL) // hits p, which eventually hits ts3
-resp4, _ := http.Get(sw.URL) // hits ts1
+resp4, _ := http.Get(sw.URL) // hits ts1 again, and so on.
 
 ```
 
-Also please check out this [introduction on Medium](https://medium.com/code-zen/go-http-test-with-relay-deade218fd3d#.ka0a2x19z).
+Also please check out this [introduction to writing test with goconvey and relay on Medium](https://medium.com/code-zen/go-http-test-with-relay-deade218fd3d#.ka0a2x19z).
 
 TODO
 ----
@@ -104,4 +143,9 @@ TODO
 + Add options to inject middleware into each proxy and switcher.
 + Add more "load-balancing" capabilities to `Switcher`.
 
-
+CONTRIBUTE
+----------
+Please feel free to open an issue or send a pull request. 
+Please see [goconvey](https://github.com/smartystreets/goconvey) on how to write BDD tests for relay.
+Contact me on twitter [@jochasinga](http://twitter.com).
+Fuel me with high-quality caffeine to continue working on cool code -> [![Flattr this git repo](http://api.flattr.com/button/flattr-badge-large.png)](https://flattr.com/submit/auto?user_id=jochasinga&url=https://github.com/jochasinga/relay&title=Relay&language=English&tags=github&category=software)
--- doc.go
+++ doc.go
@@ -3,22 +3,32 @@
 // license that can be found in the LICENSE file.
 
 /*
-Package relay is a standard httptest.Server on steroid for end-to-end HTTP testing.
+Overview
+--------
+Package relay is a standard `httptest.Server` on steroid for end-to-end HTTP testing.
+It implements the test server with a delay middleware to simulate latency before
+the target test server's handler.
 
-The basic building blocks are a Proxy and Switcher, which are HTTP Servers
-(httptest.Server) listening on a system-chosen port on the local loopback 
+Relay consists of two components, `Proxy` and `Switcher`. They are HTTP Servers
+(`httptest.Server`) listening on a system-chosen port on the local loopback 
 interface, for use in end-to-end HTTP tests.                                             
 
-A Proxy is used to place in front of any HTTPTestServer (httptest.Server, 
-Proxy, or Switcher) to simulate a proxy server or a connection with some 
-network, I/O or CPU latency. It takes a latency unit in time.Duration and 
-a backend HTTPTestServer as arguments.
+Proxy
+-----
+`Proxy` is used to place before of any `HTTPTestServer` (`httptest.Server`, 
+`Proxy`, or `Switcher`) to simulate a proxy server or a connection with some 
+network, I/O or CPU latency. It takes a latency unit in `time.Duration` and 
+a backend `HTTPTestServer` as arguments.
 
-A Switcher is a basic round-robin-style proxy which takes a latency unit
-in time.Duration and a slice of backend HTTPTestServer's among which it will
+Switcher
+--------
+`Switcher` is a basic round-robin-style proxy which takes a latency unit
+in `time.Duration` and a `[]HTTPTestServer` to which it will
 circulate requests.
 
-Let's begin setting up a basic httptest.Server to test against.
+Examples
+--------
+Let's begin setting up a basic `httptest.Server` to send request to.
 
         var handler = func(w http.ResponseWriter, r *http.Request) {
                 fmt.Fprint(w, "Hello world!")
@@ -33,46 +43,63 @@ Let's begin setting up a basic httptest.Server to test against.
                 }
         }
 
-Now, let's simulate a slow connection through which a HTTP request
-can be sent to the previous test server.
+Now, let's use Proxy to simulate a slow connection through which a HTTP request
+can be sent to the test server.
 
-        delay := time.Duration(20) * time.Second
+        // Connection takes 4s to and from the test server
+        delay := time.Duration(2) * time.Second
+        // Client takes 3s before timing out.
+        timeout = time.Duration(3) * time.Second
+        // Create a new proxy with a delay and test server backend
         conn := relay.NewProxy(delay, ts)
+        client := &Client{ Timeout: timeout }
+        start := time.Now()
+        _, _ = client.Get(conn.URL)
+        elapsed := time.Since(start)
+        deviation := time.Duration(50) * time.Millisecond
 
-        client := &Client{Timeout: time.Duration(10) * time.Second}
-        resp, _ := client.Get(conn.URL)
+        if elapsed >= timeout + deviation {
+                t.Error("Client took too long to time out.")
+        }
 
-Note that the latency will double because it takes a round trip to and from the server.
+Note that the latency will double because of the round trip to and from the server.
 
-Proxy can be placed in front of another proxy, and vice versa.
+`Proxy` can be placed in front of another proxy, and vice versa. So you can create a 
+chain of test proxies this way:
 
         delay := time.Duration(1) * time.Second
         ts := httptest.NewServer(http.HandlerFunc(handler))
         p3 := relay.NewProxy(delay, ts)
         p2 := relay.NewProxy(delay, p3)
         p1 := relay.NewProxy(delay, p2)
-        resp, _ := client.Get(p1.URL)
+        start := time.Now()
+        _, _ := http.Get(p1.URL)
+        elapsed := time.Since(start)
+        if elapsed >= time.Duration(6) * time.Second + time.Duration(10) * time.Millisecond {
+                t.Error("Client took longer than expected.")
+        }
 
 Each hop to and from the target server will be delayed for one second.
 
-Switcher works similarly to a proxy, except it "switches" between several backend servers
+`Switcher` works similarly to a proxy, except it "switches" between several backend servers
 for each request in a round-robin fashion.
 
         ts1 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
                        fmt.Fprint(w, "Hello world!")
-        }
+        }))
         ts2 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
                        fmt.Fprint(w, "Hello mars!")
-        }
+        }))
         ts3 := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
                        fmt.Fprint(w, "Hello pluto!")
-        }
+        }))
         p := relay.NewProxy(delay, ts3)
         sw := relay.NewSwitcher(delay, []HTTPTestServer{ts1, ts2, p})
 
         resp1, _ := http.Get(sw.URL) // hits ts1
         resp2, _ := http.Get(sw.URL) // hits ts2
         resp3, _ := http.Get(sw.URL) // hits p, which eventually hits ts3
+        resp4, _ := http.Get(sw.URL) // hits ts1 again
 */
 package relay
 
